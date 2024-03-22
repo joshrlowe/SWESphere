4bfca78322d9fe028cf3a561135fc76537652e23
Josh Lowe <joshlowe.cs@gmail.com>
2024-03-22 13:14:58 -0400

    Add home page templating
    Using Jinja template engine, add templating for
    the first home page of SWESphere.
    Implement a base template so future pages have
    the same features such as header and footer.
    We want this to be consistent across all pages
    of the site, so implementing a base template is
    how we can ensure this, as the site evolves.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Josh'}
+    posts = [
+        {
+            'author': {'username': 'user1'},
+            'body': 'This is user1\'s first post!'
+        },
+        {
+            'author': {'username': 'user2'},
+            'body': 'This is user2\'s first post!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
