499b08cb6bbf748e9afc503bf6e1cce8a6eae8da
Josh Lowe <joshlowe.cs@gmail.com>
2024-03-29 10:54:02 -0400

    Add login feature
    * Add login web form with form validators;
    * Add login route, which loads form to browser
      via login template;
    * Display error message to side or error messages
      below form field with error if error(s) exist
      when submitting the form.

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,31 @@
 from app import app
-from flask import render_template
+from app.forms import LoginForm
+from flask import flash, redirect, render_template, url_for
 
-@app.route('/')
-@app.route('/index')
+
+@app.route("/")
+@app.route("/index")
 def index():
-    user = {'username': 'Josh'}
+    user = {"username": "Josh"}
     posts = [
-        {
-            'author': {'username': 'user1'},
-            'body': 'This is user1\'s first post!'
-        },
-        {
-            'author': {'username': 'user2'},
-            'body': 'This is user2\'s first post!'
-        }
+        {"author": {"username": "user1"}, "body": "This is user1's first post!"},
+        {"author": {"username": "user2"}, "body": "This is user2's first post!"},
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template("index.html", title="Home", user=user, posts=posts)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(
+            f"Login requested for user {form.username.data}, remember_me={form.remember_me.data}"
+        )
+        return redirect(url_for("index"))
+    return render_template(
+        "login.html",
+        title="Sign In",
+        form=form,
+        username_errors_length=len(form.username.errors) if form.username.errors else 0,
+        password_errors_length=len(form.password.errors) if form.password.errors else 0,
+    )
