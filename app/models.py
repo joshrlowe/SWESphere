350fa1f8d713f8dad4b36877dbc7fa2f7fa6ac26
Josh Lowe <joshlowe.cs@gmail.com>
2024-04-01 09:08:29 -0400

    Add user login and logout functionality

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
+from app import login
 from datetime import datetime, timezone
+from flask_login import UserMixin
 from typing import Optional
 from werkzeug.security import generate_password_hash, check_password_hash
 import sqlalchemy as sa
@@ -6,7 +8,12 @@ import sqlalchemy.orm as so
 from app import db
 
 
-class User(db.Model):
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
+
+
+class User(db.Model, UserMixin):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
@@ -15,10 +22,10 @@ class User(db.Model):
 
     def __repr__(self) -> str:
         return f"<User {self.username}>"
-    
+
     def set_password(self, password) -> None:
         self.password_hash = generate_password_hash(password)
-    
+
     def check_password(self, password) -> None:
         return check_password_hash(self.password_hash, password)
 
--- app/routes.py
+++ app/routes.py
@@ -1,27 +1,40 @@
-from app import app
+from app import app, db
 from app.forms import LoginForm
-from flask import flash, redirect, render_template, url_for
+from app.models import User
+from flask import flash, redirect, render_template, request, url_for
+from flask_login import current_user, login_required, login_user, logout_user
+from urllib.parse import urlsplit
+import sqlalchemy as sa
 
 
 @app.route("/")
 @app.route("/index")
+@login_required
 def index():
-    user = {"username": "Josh"}
     posts = [
         {"author": {"username": "user1"}, "body": "This is user1's first post!"},
         {"author": {"username": "user2"}, "body": "This is user2's first post!"},
     ]
-    return render_template("index.html", title="Home", user=user, posts=posts)
+    return render_template("index.html", title="Home", posts=posts)
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
     form = LoginForm()
     if form.validate_on_submit():
-        flash(
-            f"Login requested for user {form.username.data}, remember_me={form.remember_me.data}"
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data)
         )
-        return redirect(url_for("index"))
+        if not user or not user.check_password(form.password.data):
+            flash("Invalid username or password")
+            return redirect(url_for("login"))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get("next")
+        if not next_page or urlsplit(next_page).netloc != "":
+            return redirect(url_for("index"))
+        return redirect(next_page)
     return render_template(
         "login.html",
         title="Sign In",
@@ -29,3 +42,9 @@ def login():
         username_errors_length=len(form.username.errors) if form.username.errors else 0,
         password_errors_length=len(form.password.errors) if form.password.errors else 0,
     )
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
