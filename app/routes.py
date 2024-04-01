ea99ceaa9436d7e8ed6d72ca2d76894060788cc5
Josh Lowe <joshlowe.cs@gmail.com>
2024-04-01 16:15:41 -0400

    Bug fix - pagination syntax

--- app/routes.py
+++ app/routes.py
@@ -53,9 +53,15 @@ def explore():
     posts = db.paginate(
         query, page=page, per_page=app.config["POSTS_PER_PAGE"], error_out=False
     )
-    next_url = url_for("explore", posts=posts.next_num) if posts.has_next else None
-    prev_url = url_for("explore", posts=posts.prev_num) if posts.has_prev else None
-    return render_template("index.html", title="Explore", posts=posts.items)
+    next_url = url_for("explore", page=posts.next_num) if posts.has_next else None
+    prev_url = url_for("explore", page=posts.prev_num) if posts.has_prev else None
+    return render_template(
+        "index.html",
+        title="Explore",
+        posts=posts.items,
+        next_url=next_url,
+        prev_url=prev_url,
+    )
 
 
 @app.route("/login", methods=["GET", "POST"])
