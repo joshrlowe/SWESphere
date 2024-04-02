b8649aa4ca59ba89fc49271cbccdf7d76504fcc0
Josh Lowe <joshlowe.cs@gmail.com>
2024-04-02 14:26:39 -0400

    Change prompt for composing a post

--- app/forms.py
+++ app/forms.py
@@ -58,6 +58,6 @@ class EmptyForm(FlaskForm):
 
 class PostForm(FlaskForm):
     post = TextAreaField(
-        "Say something", validators=[DataRequired(), Length(min=1, max=140)]
+        "What's happening?", validators=[DataRequired(), Length(min=1, max=140)]
     )
     submit = SubmitField("Submit")
