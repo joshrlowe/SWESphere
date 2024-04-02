b09a7c042575a09cd49516dfa533d257808b4822
joshrlowe <joshlowe.cs@gmail.com>
2024-04-02 13:14:45 +0000

    Make swesphere.py the entry point for the application

--- swesphere.py
+++ swesphere.py
@@ -7,3 +7,7 @@ from app.models import User, Post
 @app.shell_context_processor
 def make_shell_context():
     return {"sa": sa, "so": so, "db": db, "User": User, "Post": Post}
+
+
+if __name__ == "__main__":
+    app.run()
